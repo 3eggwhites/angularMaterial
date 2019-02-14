bcb560dee9f4ad71f3d86c9f9117f34fc0dec99c
Ayan Pal <ayangitrepo@gmail.com>
2019-02-15 01:33:59 +0530

    demo of angular material style form control elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularMaterial';
+  isChecked = false;
+
+  foods = [
+    { value: 1, viewValue: 'Burger' },
+    { value: 2, viewValue: 'Pizza' },
+    { value: 3, viewValue: 'Taco' }
+  ];
+
+  food = 3;
+
+  minDate = new Date(2019, 0, 1);
+  maxDate = new Date(2019, 7, 1);
+
+  onChnage($event) {
+    console.log($event);
+    this.isChecked = !this.isChecked;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,30 @@ import { browser } from 'protractor';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule,
     AppRoutingModule,
+    FormsModule,
     BrowserAnimationsModule,
-    MatCheckboxModule],
+    MatCheckboxModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
